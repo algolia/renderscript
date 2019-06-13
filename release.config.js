eccacaa89014582dd6fb230e03a401c904a4f332
Matthieu Dumont <jerska@live.fr>
2019-06-13 15:24:27 +0200

    chore: setup semantic-release with circleci

---
+++ release.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  branch: "master",
+  verifyConditions: ["@semantic-release/github"],
+  prepare: [
+    {
+      path: "@semantic-release/changelog",
+      changelogFile: "CHANGELOG.md"
+    },
+    "@semantic-release/npm",
+    {
+      path: "@semantic-release/git",
+      assets: ["package.json", "CHANGELOG.md"],
+      message:
+        "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }
+  ],
+  publish: "@semantic-release/github",
+  success: [],
+  fail: [],
+  npmPublish: false
+};
