816b86d339ef8aa36619cf8601bd8ca7e9c1d0ce
Matthieu Dumont <jerska@live.fr>
2019-06-14 21:36:59 +0200

    fix(ready): remove wrongly committed leftover

--- src/api/routes/ready.ts
+++ src/api/routes/ready.ts
@@ -1,10 +1,6 @@
 import * as express from "express";
 import isReady from 'lib/helpers/isReady';
 
-export async function isReady() {
-  return await renderer.ready;
-}
-
 export default async function ready(
   req: express.Request,
   res: express.Response,
