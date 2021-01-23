222ff40028d044147a2b92d2b9607859a53de9f9
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2021-01-23 16:19:25 +0100

    release with version

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+current=$(npx json -f package.json version)
+echo "Releasing: $current"
+echo ""
+
 # Reads its args from .env.prod
 if [ -f .env.prod ]; then
   source .env.prod
@@ -9,6 +13,7 @@ fi
 
 docker build \
   -t algolia/renderscript \
+  -t "algolia/renderscript:${current}" \
   --build-arg extensions_cache="$EXTENSIONS" \
   --build-arg adblock_lists_cache="$ADBLOCK_LISTS" \
   .
