96dcf6ccbf12fa7b0183c6baad0d16910fc39c0b
Sylvain Bellone <sylvain.bellone@algolia.com>
2021-04-28 12:26:37 +0200

    fix: call dotenv in command line otherwise env is set too late

--- src/helpers/dotenv.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-export default (): void => {
-  if (process.env.NODE_ENV === 'development') {
-    require('dotenv-safe').config({ allowEmptyValues: true });
-  }
-};
