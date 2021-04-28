e2feeb39c1ea976fa74188d96011ab4e0fc391ab
Sylvain Bellone <sylvain.bellone@algolia.com>
2021-04-28 12:39:30 +0200

    put back newline

--- src/lib/Renderer.ts
+++ src/lib/Renderer.ts
@@ -18,6 +18,7 @@ import { flags } from './constants';
 const IP_PREFIXES_WHITELIST = process.env.IP_PREFIXES_WHITELIST
   ? process.env.IP_PREFIXES_WHITELIST.split(',')
   : ['127.', '0.', '::1'];
+
 const RESTRICTED_IPS =
   process.env.ALLOW_LOCALHOST === 'true'
     ? PRIVATE_IP_PREFIXES.filter(
