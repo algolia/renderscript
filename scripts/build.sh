39b6478d12eeba91ac88cb18bee58506ec2fae39
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2021-07-22 11:11:02 +0200

    fix: remove env sourcing

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,11 +6,6 @@ current=$(node -e "console.log(require('./package.json').version)")
 echo "Releasing: $current"
 echo ""
 
-# Reads its args from .env.prod
-if [ -f .env.prod ]; then
-  source .env.prod
-fi
-
 docker build \
   -t algolia/renderscript \
   -t "algolia/renderscript:${current}" \
