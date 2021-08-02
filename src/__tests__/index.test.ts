13cfb77c8ffbdb9acf844a88ad71344ddcfd6102
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2021-08-02 09:47:49 +0200

    increase metrics tolerance

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -143,8 +143,8 @@ describe('main', () => {
     expect(json.metrics.serialize).toBeLessThanOrEqual(20);
     expect(json.metrics.total).toBeLessThanOrEqual(2100);
     expect(json.metrics.page.layoutDuration).toBeLessThanOrEqual(2);
-    expect(json.metrics.page.scriptDuration).toBeLessThanOrEqual(1);
-    expect(json.metrics.page.taskDuration).toBeLessThanOrEqual(10);
+    expect(json.metrics.page.scriptDuration).toBeLessThanOrEqual(2);
+    expect(json.metrics.page.taskDuration).toBeLessThanOrEqual(20);
     expect(json.metrics.page.jsHeapUsedSize).toBeLessThanOrEqual(1617360);
     expect(json.metrics.page.jsHeapTotalSize).toBeLessThanOrEqual(2639520);
     expect(json.metrics.page.requests).toEqual(1);
