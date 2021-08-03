12af9a221096eaedad7364de93e3c4c0ef29a95e
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2021-08-03 22:30:05 +0200

    test: increase threshold

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -142,9 +142,9 @@ describe('main', () => {
     expect(json.metrics.minWait).toBeLessThanOrEqual(1100);
     expect(json.metrics.serialize).toBeLessThanOrEqual(20);
     expect(json.metrics.total).toBeLessThanOrEqual(2100);
-    expect(json.metrics.page.layoutDuration).toBeLessThanOrEqual(2);
-    expect(json.metrics.page.scriptDuration).toBeLessThanOrEqual(2);
-    expect(json.metrics.page.taskDuration).toBeLessThanOrEqual(20);
+    expect(json.metrics.page.layoutDuration).toBeLessThanOrEqual(10);
+    expect(json.metrics.page.scriptDuration).toBeLessThanOrEqual(10);
+    expect(json.metrics.page.taskDuration).toBeLessThanOrEqual(30);
     expect(json.metrics.page.jsHeapUsedSize).toBeLessThanOrEqual(1617360);
     expect(json.metrics.page.jsHeapTotalSize).toBeLessThanOrEqual(2639520);
     expect(json.metrics.page.requests).toEqual(1);
