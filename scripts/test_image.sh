acf00756070332ab983fe85f569bbd4d00a13d91
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2022-03-16 10:44:38 +0100

    unnecessary

--- scripts/test_image.sh
+++ scripts/test_image.sh
@@ -3,7 +3,7 @@
 set -ex
 
 hash=$(git rev-parse HEAD) # the last commit change because of semantic-release
-docker run -d --name renderscript_test -p 3000:3000 docker.io/algolia/renderscript:$hash
+docker run -d --name renderscript_test -p 3000:3000 algolia/renderscript:$hash
 
 echo "waiting for docker"
 sleepSec=10
