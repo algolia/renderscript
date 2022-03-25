db395fd7f6be80cb308942f6ab980da235516b67
Samuel Bodin <samuel.bodin@algolia.com>
2022-03-25 18:05:48 +0100

    fix(docker): improve dockerfile size (#592)

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,17 @@ current=$(node -e "console.log(require('./package.json').version)")
 echo "Releasing: $current"
 echo ""
 
+# Build base image
+
+docker build \
+  --progress plain \
+  -t algolia/renderscript-pw-chromium:${current} \
+  --build-arg "VERSION=${current}" \
+  -f Dockerfile.pw \
+  .
+
+# Build renderscript
+
 docker build \
   --progress plain \
   -t algolia/renderscript \
