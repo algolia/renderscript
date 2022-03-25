03b9aed55455adc1a4f12c1199bc5bfca051256d
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2022-03-25 19:44:23 +0100

    combine image for simplicity

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,16 +7,6 @@ current=$(node -e "console.log(require('./package.json').version)")
 echo "Releasing: $current"
 echo ""
 
-# Build base image
-
-docker buildx build \
-  --platform linux/arm64/v8,linux/amd64 \
-  --progress plain \
-  -t algolia/renderscript-pw-chromium:${current} \
-  --build-arg "VERSION=${current}" \
-  -f Dockerfile.pw \
-  .
-
 # Build renderscript
 
 docker buildx build \
