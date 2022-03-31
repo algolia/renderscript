b29de34ac1e300600cd847ddee249eb87ad4935a
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2022-03-31 14:39:22 +0200

    fix: unnecessary dot

--- src/lib/browser/constants.ts
+++ src/lib/browser/constants.ts
@@ -7,7 +7,7 @@ export const RESPONSE_IGNORED_ERRORS = [
   // Protocol error, js redirect or options
   'This might happen if the request is a preflight request',
   // Can happen if the page that trigger this response was closed in the meantime
-  'Target closed.',
+  'Target closed',
 ];
 
 export const REQUEST_IGNORED_ERRORS = ['Request is already handled'];
