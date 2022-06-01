c1402d311c1e8da4982261e194f6995c4e863650
Samuel Bodin <samuel.bodin@algolia.com>
2022-06-01 16:19:09 +0200

    fix(api): catch all at the end + 404 + / in prod (#613)

---
+++ src/api/routes/root.ts
@@ -0,0 +1,10 @@
+import type express from 'express';
+
+import type { GetRoot } from '../@types/getRoot';
+
+export function root(
+  req: express.Request,
+  res: express.Response<GetRoot>
+): void {
+  res.status(200).json({ version: process.env.VERSION || 'dev' });
+}
