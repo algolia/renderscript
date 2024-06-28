d594988ecf2dfc8597a980393d1259d6b5b1d466
Dylan Tientcheu <dylantientcheu@gmail.com>
2024-06-28 11:40:18 +0200

    fix: save only on compile

--- scripts/update_adblock_hosts.sh
+++ scripts/update_adblock_hosts.sh
@@ -3,19 +3,10 @@
 # adblock hosts file URL
 URL="https://raw.githubusercontent.com/badmojr/1Hosts/master/Pro/domains.txt"
 
-TARGET_DIR="./src/lib/browser"
-DIST_DIR="./dist/lib/browser"
+TARGET_DIR="./dist/lib/browser"
 TARGET_FILE="adblock_hosts.txt"
 
-# check if the target and dist directory exists
-# should always be true, but just in case
-if [ ! -d "$TARGET_DIR" ] || [ ! -d "$DIST_DIR" ]; then
-  echo "❌ target or dist directory does not exist."
-  exit 1
-fi
-
-# download the adblock hosts file
-if curl -o "${TARGET_DIR}/${TARGET_FILE}" "$URL" -s && cp "${TARGET_DIR}/${TARGET_FILE}" "${DIST_DIR}/${TARGET_FILE}"; then
+if curl -o "${TARGET_DIR}/${TARGET_FILE}" "$URL" -s; then
   echo "✅ adblock hosts download successful."
 else
   echo "❌ adblock hosts download failed."
