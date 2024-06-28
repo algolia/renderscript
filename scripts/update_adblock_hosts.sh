86896e486bb8a02ed4e9a43a7a670f99ed944106
Dylan Tientcheu <dylantientcheu@gmail.com>
2024-06-28 11:08:24 +0200

    fix: copy txt file during build

--- scripts/update_adblock_hosts.sh
+++ scripts/update_adblock_hosts.sh
@@ -4,11 +4,18 @@
 URL="https://raw.githubusercontent.com/badmojr/1Hosts/master/Pro/domains.txt"
 
 TARGET_DIR="./src/lib/browser"
+DIST_DIR="./dist/lib/browser"
 TARGET_FILE="adblock_hosts.txt"
 
-mkdir -p "$TARGET_DIR"
+# check if the target and dist directory exists
+# should always be true, but just in case
+if [ ! -d "$TARGET_DIR" ] || [ ! -d "$DIST_DIR" ]; then
+  echo "❌ target or dist directory does not exist."
+  exit 1
+fi
 
-if curl -o "${TARGET_DIR}/${TARGET_FILE}" "$URL" -s; then
+# download the adblock hosts file
+if curl -o "${TARGET_DIR}/${TARGET_FILE}" "$URL" -s && cp "${TARGET_DIR}/${TARGET_FILE}" "${DIST_DIR}/${TARGET_FILE}"; then
   echo "✅ adblock hosts download successful."
 else
   echo "❌ adblock hosts download failed."
