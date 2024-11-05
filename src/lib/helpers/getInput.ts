f7df4ebd4ea29ee64ffa22fc84ec9399b1cd41e3
Dylan Tientcheu <dylantientcheu@gmail.com>
2024-11-05 11:33:01 +0100

    fix: account for invisible password input fields

--- src/lib/helpers/getInput.ts
+++ src/lib/helpers/getInput.ts
@@ -4,13 +4,21 @@ import type { BrowserPage } from '../browser/Page';
 import type { HandledError } from '../types';
 
 /**
+ * Check if an element is visible.
+ */
+function isVisible(element: Element): boolean {
+  return window.getComputedStyle(element).display !== 'none';
+}
+
+/**
  * Get input for selector.
  */
 export async function getInput(
   page: BrowserPage | undefined,
   sel: string
 ): Promise<Locator | { error: HandledError; rawError: Error }> {
-  const textInputLoc = page?.ref?.locator(sel).locator('visible=true');
+  const textInputLoc = page?.ref?.locator(sel);
+
   const count = textInputLoc ? await textInputLoc.count() : 0;
   if (!textInputLoc || count <= 0) {
     return {
@@ -20,12 +28,27 @@ export async function getInput(
   }
 
   if (count > 1) {
-    return {
-      error: 'too_many_fields',
-      rawError: new Error(
-        `Too many input found for "${sel}", found "${count}"`
-      ),
-    };
+    // sometimes there are multiple input fields with the same selector
+    // we want to find out if there's a visible one
+    const visibleElements = await textInputLoc!.evaluateAll((elements) => {
+      return elements.filter(isVisible);
+    });
+
+    if (visibleElements.length !== 1) {
+      return {
+        error: 'too_many_fields',
+        rawError: new Error(
+          `Too many input found for "${sel}", found "${visibleElements.length}"`
+        ),
+      };
+    }
+
+    const visibleTextInputLoc = textInputLoc.nth(
+      await textInputLoc.evaluateAll((elements) =>
+        elements.findIndex(isVisible)
+      )
+    );
+    return visibleTextInputLoc;
   }
 
   return textInputLoc;
