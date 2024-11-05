8198aa34ec3fb8310927fbb443d546254400d1b6
Dylan Tientcheu <dylantientcheu@gmail.com>
2024-11-05 11:50:35 +0100

    fix: handle hidden inputs

--- src/lib/helpers/getInput.ts
+++ src/lib/helpers/getInput.ts
@@ -4,13 +4,6 @@ import type { BrowserPage } from '../browser/Page';
 import type { HandledError } from '../types';
 
 /**
- * Check if an element is visible.
- */
-function isVisible(element: Element): boolean {
-  return window.getComputedStyle(element).display !== 'none';
-}
-
-/**
  * Get input for selector.
  */
 export async function getInput(
@@ -28,27 +21,35 @@ export async function getInput(
   }
 
   if (count > 1) {
-    // sometimes there are multiple input fields with the same selector
-    // we want to find out if there's a visible one
-    const visibleElements = await textInputLoc!.evaluateAll((elements) => {
-      return elements.filter(isVisible);
-    });
+    try {
+      // sometimes the input is hidden because the page is not fully loaded
+      // wait for the page to be fully loaded
+      await page?.waitForNavigation({
+        waitUntil: 'load',
+        timeout: 10000,
+      });
 
-    if (visibleElements.length !== 1) {
+      // check again but this time only for visible elements
+      const visibleInputLoc = await textInputLoc.locator('visible=true');
+      const visibleCount = visibleInputLoc ? await visibleInputLoc.count() : 0;
+      if (visibleCount === 1) {
+        return visibleInputLoc;
+      }
+
       return {
         error: 'too_many_fields',
         rawError: new Error(
-          `Too many input found for "${sel}", found "${visibleElements.length}"`
+          `Too many input found for "${sel}", found "${count}"`
+        ),
+      };
+    } catch (error) {
+      return {
+        error: 'page_timeout',
+        rawError: new Error(
+          `Page timeout while looking for input "${sel}", found "${count}"`
         ),
       };
     }
-
-    const visibleTextInputLoc = textInputLoc.nth(
-      await textInputLoc.evaluateAll((elements) =>
-        elements.findIndex(isVisible)
-      )
-    );
-    return visibleTextInputLoc;
   }
 
   return textInputLoc;
