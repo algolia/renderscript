53fcfa1782f2f0c64f4de5a4d8efa8067fb33e06
Dylan Tientcheu <dylantientcheu@gmail.com>
2025-04-08 16:15:36 +0200

    fix: invalid url catch

--- src/lib/helpers/validateURL.ts
+++ src/lib/helpers/validateURL.ts
@@ -6,6 +6,14 @@ import { RESTRICTED_IPS } from '../constants';
 
 export async function isURLAllowed(url: string): Promise<boolean> {
   try {
+    // Check for valid URL before validation
+    // eslint-disable-next-line no-new
+    new URL(url);
+  } catch (e) {
+    report(new Error('Invalid url'), { url, err: e });
+    return false;
+  }
+  try {
     await validateURL({
       url,
       ipPrefixes: RESTRICTED_IPS,
